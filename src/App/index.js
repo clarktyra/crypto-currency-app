a3f31c80126666704bdc79e93eb276163ed7263d
Clark Tyra <clark.tyra@gmail.com>
2019-06-27 22:17:53 -0700

    added content for loading

--- src/App/index.js
+++ src/App/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import styled, { css } from 'styled-components';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
-import {AppProvider} from './AppProvider'
-import Settings from '../Settings'
+import { AppProvider } from './AppProvider';
+import Settings from '../Settings';
+import Content from '../Shared/Content';
 
-function App() {
-  return (
-    <AppLayout>
-      <AppProvider>
-        <AppBar></AppBar>
-        <Settings />
-      </AppProvider>
-    </AppLayout>
-  );
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <AppProvider>
+          <AppBar/>
+          <Content>
+            <Settings />
+          </Content>
+        </AppProvider>
+      </AppLayout>
+    );
+  }
 }
 
 export default App;
--- src/Settings/WelcomeMessage.js
+++ src/Settings/WelcomeMessage.js
@@ -7,7 +7,7 @@ export default function WelcomeMessage(props) {
         {({firstVisit}) =>
         firstVisit ? <div>
         Welcome to CryptoDash, please select your favorite coins to begin. {' '}
-        </div> : null
+        </div> : <div>hi</div>
       }
       </AppContext.Consumer>
     )
