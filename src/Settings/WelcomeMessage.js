54494a093640f0f2a281974384a96f403d301f91
Clark Tyra <clark.tyra@gmail.com>
2019-06-30 13:53:10 -0700

    Finish Add/Remove Coins

--- src/Settings/WelcomeMessage.js
+++ src/Settings/WelcomeMessage.js
@@ -7,7 +7,7 @@ export default function WelcomeMessage(props) {
         {({firstVisit}) =>
         firstVisit ? <div>
         Welcome to CryptoDash, please select your favorite coins to begin. {' '}
-        </div> : <div>hi</div>
+        </div> : <div></div>
       }
       </AppContext.Consumer>
     )
